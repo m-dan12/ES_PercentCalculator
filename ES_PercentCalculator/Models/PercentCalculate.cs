ba6ca06a552dee9a75857d8129d7173f4f3af31c
Даниил Малинин <m-dan12@ya.ru>
2024-05-28 01:01:25 +0400

    Пока соси соси

--- ES_PercentCalculator/Models/PercentCalculate.cs
+++ ES_PercentCalculator/Models/PercentCalculate.cs
@@ -12,13 +12,14 @@ namespace ES_PercentCalculator.Models
         // Метод для вычисления процентной ставки
         public static double CalculateInterestRate(double P, double S, int n)
         {
-            double r = 0.05;  // начальное приближение
+            double r = 10;  // начальное приближение
             for (int i = 0; i < 1000; i++)
             {
                 double f = P * (Math.Pow(1 + r, n) - 1) - S * r * Math.Pow(1 + r, n);
                 double f_prime = P * n * Math.Pow(1 + r, n - 1) - S * (Math.Pow(1 + r, n) + r * n * Math.Pow(1 + r, n - 1));
+                if (f_prime == 0) break;
                 double r_new = r - f / f_prime;
-                if (Math.Abs(r_new - r) < 1e-10)
+                if (Math.Abs(r_new - r) < 1e-6)
                     break;
                 r = r_new;
             }
@@ -26,53 +27,3 @@ namespace ES_PercentCalculator.Models
         }
     }
 }
-
-/*static double CalculateInterestRate(double S, double P, int n)
-{
-    double r = 0.05; // начальное приближение
-    for (int i = 0; i < 1000; i++)
-    {
-        double f = P * (Math.Pow(1 + r, n) - 1) - S * r * (Math.Pow(1 + r, n));
-        double f_prime = P * n * Math.Pow(1 + r, (n - 1)) - S * (Math.Pow(1 + r, n) + r * n * Math.Pow(1 + r, (n - 1)));
-        double r_new = r - f / f_prime;
-        if (Math.Abs(r_new - r) < 1e-10)
-        {
-            break;
-        }
-        r = r_new;
-    }
-    return r * 12 * 100; // годовая процентная ставка в процентах
-}*/
-/* Пример использовния
-class Program
-{
-    static void Main()
-    {
-        double S = 100000;  // сумма кредита
-        double P = 3000;    // ежемесячный платеж
-        int n = 36;         // количество месяцев
-
-        PercentCalculate percentCalculate = new PercentCalculate(S, P, n);
-        double annualInterestRate = percentCalculate.CalculateInterestRate();
-        Console.WriteLine($"Годовая процентная ставка: {annualInterestRate:F2}%");
-    }
-}*/
-
-/* Python Code
-    def calculate_interest_rate(S, P, n):
-    r = 0.05  # начальное приближение
-    for i in range(1000):
-        f = P * ((1 + r) * *n - 1) - S * r * (1 + r) * *n
-        f_prime = P * n * (1 + r) * *(n - 1) - S * ((1 + r) * *n + r * n * (1 + r) * *(n - 1))
-        r_new = r - f / f_prime
-        if abs(r_new - r) < 1e-10:
-            break
-        r = r_new
-    return r * 12 * 100  # годовая процентная ставка в процентах
-
-S = 100000  # сумма кредита
-n = 36      # количество месяцев
-P = 3000    # ежемесячный платеж
-
-annual_interest_rate = calculate_interest_rate(S, P, n)
-print(f"Годовая процентная ставка: {annual_interest_rate:.2f}%")*/
--- ES_PercentCalculator/ViewModels/MainWindowViewModel.cs
+++ ES_PercentCalculator/ViewModels/MainWindowViewModel.cs
@@ -10,37 +10,41 @@ namespace ES_PercentCalculator.ViewModels
         public MainWindowViewModel()
         {
             InputData = new();
-            Current = new(InputData, null, (18.2, 17.2, 16.7));
-            New_Period1 = new(InputData, InputData.ConstructionPeriod * 12, (14.2, 13.2, 12.7));
+            Current = new(InputData, (18.2, 17.2, 16.7));
+            New_Period1 = new(InputData, (14.2, 13.2, 12.7));
             New_Period2 = new(InputData, Current, New_Period1);
         }
         [Reactive] public Input InputData { get; set; }
-        [Reactive] public Program Current { get; set; }
-        [Reactive] public Program New_Period1 { get; set; }
-        [Reactive] public NewProgramLastPeriod New_Period2 { get; set; }
+        [Reactive] public Class_Current Current { get; set; }
+        [Reactive] public Class_New_Period1 New_Period1 { get; set; }
+        [Reactive] public Class_New_Period2 New_Period2 { get; set; }
+        
+        // Существующая программа сбера
         public class Input : ReactiveObject
         {
-            public const int MinimumWage = 13890;
+            public const int MinimumWage = 15453;                                           // минимальный прожиточный минимум
 
-            [Reactive] public int PropertyValue { get; set; } = 3000000;                        // стоимость недвижимости
-            [Reactive] public int InitialFee { get; set; } = 1000000;                         // первоначальный взнос
-            [Reactive] public int CreditTerm { get; set; } = 10;                             // срок кредита (1-30 лет)
+            [Reactive] public int PropertyValue { get; set; } = 3000000;                    // стоимость недвижимости
+            [Reactive] public int InitialFee { get; set; } = 1000000;                       // первоначальный взнос
+            [Reactive] public int CreditTerm { get; set; } = 4;                             // срок кредита (1-30 лет)
             [Reactive] public int Rent { get; set;} = 25000;
             [Reactive] public int ConstructionPeriod { get; set;} = 3;
             [ObservableAsProperty] public int InitialFeeMin { get; }                        // минимальное значение первоначального взноса в зависимости от стоимости недвижимости
             [ObservableAsProperty] public int InitialFeeMax { get; }                        // максимальное значение первоначального взноса в зависимости от стоимости недвижимости
+            [ObservableAsProperty] public int MinimumCreditTerm { get; }                    // срок кредита (1-30 лет)
+            [ObservableAsProperty] public string? MinimumCreditTerm_Performance { get; }    // срок кредита (1-30 лет)
             [ObservableAsProperty] public string? InitialFeeMin_Performance { get; }        // минимальное значение первоначального взноса в зависимости от стоимости недвижимости
             [ObservableAsProperty] public string? InitialFeeMax_Performance { get; }        // максимальное значение первоначального взноса в зависимости от стоимости недвижимости
             [ObservableAsProperty] public double InitialFeeInPercent { get; }               // первоначальный взнос (в процентах)
-            
+            [ObservableAsProperty] public string? InitialFeeInPercent_Performance { get; }  // первоначальный взнос (в процентах)
             public Input()
             {
                 this.WhenAnyValue(vm => vm.PropertyValue)
                     .Select(x => (int)(x * 0.151))
                     .ToPropertyEx(this, vm => vm.InitialFeeMin);
-
+                
                 this.WhenAnyValue(vm => vm.InitialFeeMin)
-                    .Select(value => value >= 1000000 ? $"{value / 1000000.0:F1} млн. Р" : $"{value / 1000.0:F1} тыс. Р")
+                    .Select(value => value >= 1000000 ? $"{value / 1000000.0:F1} млн. ₽" : $"{value / 1000.0:F1} тыс. ₽")
                     .ToPropertyEx(this, vm => vm.InitialFeeMin_Performance);
 
                 this.WhenAnyValue(vm => vm.PropertyValue)
@@ -48,26 +52,101 @@ namespace ES_PercentCalculator.ViewModels
                     .ToPropertyEx(this, vm => vm.InitialFeeMax);
 
                 this.WhenAnyValue(vm => vm.InitialFeeMax)
-                    .Select(value => value >= 1000000 ? $"{value / 1000000.0:F1} млн. Р" : $"{value / 1000.0:F1} тыс. Р")
+                    .Select(value => value >= 1000000 ? $"{value / 1000000.0:F1} млн. ₽" : $"{value / 1000.0:F1} тыс. ₽")
                     .ToPropertyEx(this, vm => vm.InitialFeeMax_Performance);
 
                 this.WhenAnyValue(vm => vm.InitialFee,
                                   vm => vm.PropertyValue,
-                                  (initialFee, propertyValue) => Math.Round(initialFee / (double)propertyValue * 100, 1))
+                                  (initialFee, propertyValue) => initialFee / (double)propertyValue * 100)
                     .ToPropertyEx(this, vm => vm.InitialFeeInPercent);
 
-                
+                this.WhenAnyValue(vm => vm.ConstructionPeriod)
+                    .Select(x => x + 1)
+                    .ToPropertyEx(this, vm => vm.MinimumCreditTerm);
+
+                this.WhenAnyValue(vm => vm.InitialFeeInPercent).Select(x => $"{x:F1}%").ToPropertyEx(this, vm => vm.InitialFeeInPercent_Performance);
+                this.WhenAnyValue(vm => vm.MinimumCreditTerm).Select(x => x % 10 == 1 && x % 100 != 11     ? $"{x} год"  :
+                                                                          x % 10 > 1 && x % 10 <= 4 &&
+                                                                          !(x % 100 > 10 && x % 100 <= 14) ? $"{x} года" :
+                                                                          $"{x} лет").ToPropertyEx(this, vm => vm.MinimumCreditTerm_Performance);
             }
         }
-        public class Program : ReactiveObject
+        public class Class_Current : ReactiveObject
         {
-            private Input _input;
-            private double _monthlyRate => InterestRate / 12;
+            private readonly Input _input;                                                                    // инициализация входных данных
+            
 
-            private int? _numberOfMonth;
-            public int NumberOfMonth => _numberOfMonth ?? _input.CreditTerm * 12;
+            #region Рабочие переменные
+            [ObservableAsProperty] public double InterestRate { get; }                                        // годовая ставка
+            [ObservableAsProperty] public double MonthlyPayment { get; }                                      // ежемесячный платеж
+            [ObservableAsProperty] public int CreditAmount { get; }                                           // сумма кредита
+            [ObservableAsProperty] public double RequiredIncomeForConstructionPeriod { get; }                 // необходимый доход для периода постройки
+            [ObservableAsProperty] public double RequiredIncomeForOtherPeriod { get; }                        // необходимый доход для оставшегося периода
+            [ObservableAsProperty] public double FinalOverpayment { get; }                                    // итоговая переплата
+            #endregion
 
-            private (double, double, double) _percents; // от большего к меньшему
+            #region Переменные представления
+            [ObservableAsProperty] public string? InterestRate_Performance { get; }                           // годовая ставка
+            [ObservableAsProperty] public string? MonthlyPayment_Performance { get; }                         // ежемесячный платеж
+            [ObservableAsProperty] public string? CreditAmount_Performance { get; }                           // сумма кредита
+            [ObservableAsProperty] public string? RequiredIncomeForConstructionPeriod_Performance { get; }    // необходимый доход для периода постройки
+            [ObservableAsProperty] public string? RequiredIncomeForOtherPeriod_Performance { get; }           // необходимый доход для оставшегося периода
+            [ObservableAsProperty] public string? FinalOverpayment_Performance { get; }                       // итоговая переплата
+            #endregion
+
+            #region Вспомогательные переменные
+            private (double, double, double) _percents;
+            private double MonthlyRate => InterestRate / 12;
+            #endregion
+            public Class_Current(Input input, (double, double, double) percents)
+            {
+                _input = input;
+                _percents = percents;
+
+                this.WhenAnyValue(vm => vm._input.InitialFeeInPercent)
+                    .Select(x => x <= 20 ? _percents.Item1 : x <= 30 ? _percents.Item2 : _percents.Item3)
+                    .ToPropertyEx(this, vm => vm.InterestRate);
+
+                this.WhenAnyValue(vm => vm._input.PropertyValue,
+                              vm => vm._input.InitialFee,
+                              (propertyValue, initialFee) => propertyValue - initialFee)
+                .ToPropertyEx(this, vm => vm.CreditAmount);
+
+                this.WhenAnyValue(vm => vm.InterestRate,
+                                  vm => vm.CreditAmount,
+                                  vm => vm._input.CreditTerm,
+                                  (interestRate, creditAmount, creditTerm) => MonthlyPaymentCalculate(creditAmount, MonthlyRate / 100, creditTerm * 12))
+                    .ToPropertyEx(this, vm => vm.MonthlyPayment);
+
+                this.WhenAnyValue(vm => vm.MonthlyPayment,
+                                  vm => vm._input.Rent,
+                                  (monthlyPayment, rent) => monthlyPayment + rent + Input.MinimumWage)
+                    .ToPropertyEx(this, vm => vm.RequiredIncomeForConstructionPeriod);
+
+                this.WhenAnyValue(vm => vm.MonthlyPayment,
+                                  vm => vm._input.Rent,
+                                  (monthlyPayment, rent) => monthlyPayment + Input.MinimumWage)
+                    .ToPropertyEx(this, vm => vm.RequiredIncomeForOtherPeriod);
+
+                this.WhenAnyValue(vm => vm.MonthlyPayment,
+                                  vm => vm.CreditAmount,
+                                  vm => vm._input.CreditTerm,
+                                  (monthlyPayment, creditAmount, creditTerm) => creditTerm * 12 * monthlyPayment - creditAmount)
+                    .ToPropertyEx(this, vm => vm.FinalOverpayment);
+
+                this.WhenAnyValue(vm => vm.InterestRate).Select(x => $"{x:F1}%").ToPropertyEx(this, vm => vm.InterestRate_Performance);
+                this.WhenAnyValue(vm => vm.MonthlyPayment).Select(x => $"{x:F0} ₽").ToPropertyEx(this, vm => vm.MonthlyPayment_Performance);
+                this.WhenAnyValue(vm => vm.CreditAmount).Select(x => $"{x} ₽").ToPropertyEx(this, vm => vm.CreditAmount_Performance);
+                this.WhenAnyValue(vm => vm.RequiredIncomeForConstructionPeriod).Select(x => $"{x:F0} ₽").ToPropertyEx(this, vm => vm.RequiredIncomeForConstructionPeriod_Performance);
+                this.WhenAnyValue(vm => vm.RequiredIncomeForOtherPeriod).Select(x => $"{x:F0} ₽").ToPropertyEx(this, vm => vm.RequiredIncomeForOtherPeriod_Performance);
+                this.WhenAnyValue(vm => vm.FinalOverpayment).Select(x => $"{x:F0} ₽").ToPropertyEx(this, vm => vm.FinalOverpayment_Performance);
+            }
+            private static double MonthlyPaymentCalculate(int creditAmount, double monthlyRate, int numberOfMonth) =>
+                creditAmount * (monthlyRate * Math.Pow(1 + monthlyRate, numberOfMonth)) / (Math.Pow(1 + monthlyRate, numberOfMonth) - 1);
+        }
+        public class Class_New_Period1 : ReactiveObject
+        {
+            private readonly Input _input;
 
             #region Рабочие переменные
             [ObservableAsProperty] public double InterestRate { get; }      // годовая ставка
@@ -78,17 +157,21 @@ namespace ES_PercentCalculator.ViewModels
             #endregion
 
             #region Переменные представления
-            [ObservableAsProperty] public string? InterestRate_Performance { get; }      // годовая ставка
-            [ObservableAsProperty] public string? MonthlyPayment_Performance { get; }    // ежемесячный платеж
-            [ObservableAsProperty] public string? CreditAmount_Performance { get; }      // сумма кредита
-            [ObservableAsProperty] public string? RequiredIncome_Performance { get; }    // необходимый доход
-            [ObservableAsProperty] public string? FinalOverpayment_Performance { get; }  // итоговая переплата
+            [ObservableAsProperty] public string? InterestRate_Performance { get; }     // годовая ставка
+            [ObservableAsProperty] public string? MonthlyPayment_Performance { get; }   // ежемесячный платеж
+            [ObservableAsProperty] public string? CreditAmount_Performance { get; }     // сумма кредита
+            [ObservableAsProperty] public string? RequiredIncome_Performance { get; }   // необходимый доход
+            [ObservableAsProperty] public string? FinalOverpayment_Performance { get; } // итоговая переплата
             #endregion
 
-            public Program(Input input, int? numberOfMonth, (double, double, double) percents)
+            #region Вспомогательные переменные
+            private (double, double, double) _percents;         // от большего к меньшему
+            private double MonthlyRate => InterestRate / 12;    // месечная ставка
+            #endregion
+
+            public Class_New_Period1(Input input, (double, double, double) percents)
             {
                 _input = input;
-                _numberOfMonth = numberOfMonth;
                 _percents = percents;
 
                 this.WhenAnyValue(vm => vm._input.InitialFeeInPercent)
@@ -98,14 +181,15 @@ namespace ES_PercentCalculator.ViewModels
                 this.WhenAnyValue(vm => vm._input.PropertyValue,
                               vm => vm._input.InitialFee,
                               vm => vm._input.CreditTerm,
-                              (propertyValue, initialFee, creditTerm) => (value: propertyValue - initialFee, allNumberOfMonth: creditTerm * 12))
-                .Select(x => NumberOfMonth != numberOfMonth ? x.value : x.value * NumberOfMonth / x.allNumberOfMonth)
+                              vm => vm._input.ConstructionPeriod,
+                              (propertyValue, initialFee, creditTerm, constructPeriod) => (propertyValue - initialFee) / creditTerm * constructPeriod)
                 .ToPropertyEx(this, vm => vm.CreditAmount);
 
                 this.WhenAnyValue(vm => vm.InterestRate,
                                   vm => vm.CreditAmount,
                                   vm => vm._input.CreditTerm,
-                                  (interestRate, creditAmount, creditTerm) => MonthlyPaymentCalculate(creditAmount, _monthlyRate / 100, NumberOfMonth))
+                                  vm => vm._input.ConstructionPeriod,
+                                  (interestRate, creditAmount, creditTerm, constructPeriod) => MonthlyPaymentCalculate(creditAmount, MonthlyRate / 100, constructPeriod * 12))
                     .ToPropertyEx(this, vm => vm.MonthlyPayment);
 
                 this.WhenAnyValue(vm => vm.MonthlyPayment,
@@ -116,7 +200,8 @@ namespace ES_PercentCalculator.ViewModels
                 this.WhenAnyValue(vm => vm.MonthlyPayment,
                                   vm => vm.CreditAmount,
                                   vm => vm._input.CreditTerm,
-                                  (monthlyPayment, creditAmount, creditTerm) => NumberOfMonth * monthlyPayment - creditAmount)
+                                  vm => vm._input.ConstructionPeriod,
+                                  (monthlyPayment, creditAmount, creditTerm, constructPeriod) => constructPeriod * 12 * monthlyPayment - creditAmount)
                     .ToPropertyEx(this, vm => vm.FinalOverpayment);
 
                 this.WhenAnyValue(vm => vm.InterestRate).Select(x => $"{x:F1}%").ToPropertyEx(this, vm => vm.InterestRate_Performance);
@@ -126,20 +211,14 @@ namespace ES_PercentCalculator.ViewModels
                 this.WhenAnyValue(vm => vm.FinalOverpayment).Select(x => $"{x:F0} ₽").ToPropertyEx(this, vm => vm.FinalOverpayment_Performance);
             }
             private static double MonthlyPaymentCalculate(int creditAmount, double monthlyRate, int numberOfMonth) =>
-                creditAmount * (monthlyRate * Math.Pow(1 + monthlyRate, numberOfMonth)) / (Math.Pow(1 + monthlyRate, numberOfMonth) - 1);
+                (monthlyRate * Math.Pow(1 + monthlyRate, numberOfMonth)) / (Math.Pow(1 + monthlyRate, numberOfMonth) - 1) * creditAmount;
         }
-        public class NewProgramLastPeriod : ReactiveObject
+        public class Class_New_Period2 : ReactiveObject
         {
-            private Input _input;
-            private Program _current;
-            private Program _new_Period1;
-
-            //private double _monthlyRate => InterestRate / 12;
-
-            private int? _numberOfMonth;
-            public int NumberOfMonth => _numberOfMonth ?? _current.NumberOfMonth - _new_Period1.NumberOfMonth;
-
-            private (double, double, double) _percents; // от большего к меньшему
+            private readonly Input _input;
+            private readonly Class_Current _current;
+            private readonly Class_New_Period1 _new_Period1;
+            private int NumberOfMonth => (_input.CreditTerm - _input.ConstructionPeriod) * 12;
 
             #region Рабочие переменные
             [ObservableAsProperty] public double InterestRate { get; }      // годовая ставка
@@ -157,7 +236,7 @@ namespace ES_PercentCalculator.ViewModels
             [ObservableAsProperty] public string? FinalOverpayment_Performance { get; }  // итоговая переплата
             #endregion
 
-            public NewProgramLastPeriod(Input input, Program current, Program new_Period1)
+            public Class_New_Period2(Input input, Class_Current current, Class_New_Period1 new_Period1)
             {
                 _input = input;
                 _current = current;
@@ -177,13 +256,13 @@ namespace ES_PercentCalculator.ViewModels
                                   vm => vm.FinalOverpayment,
                                   (creditAmount, finalOverpayment) => (creditAmount + finalOverpayment) / NumberOfMonth)
                     .ToPropertyEx(this, vm => vm.MonthlyPayment);
-                
+
                 this.WhenAnyValue(vm => vm.MonthlyPayment,
                                   vm => vm._input.Rent,
-                                  (monthlyPayment, rent) => monthlyPayment + rent + Input.MinimumWage)
+                                  (monthlyPayment, rent) => monthlyPayment + Input.MinimumWage)
                     .ToPropertyEx(this, vm => vm.RequiredIncome);
 
-                this.WhenAnyValue(vm => vm.MonthlyPayment, 
+                this.WhenAnyValue(vm => vm.MonthlyPayment,
                                   vm => vm.CreditAmount,
                                   (monthlyPayment, creditAmount) => Models.PercentCalculate.CalculateInterestRate(monthlyPayment, creditAmount, NumberOfMonth))
                     .ToPropertyEx(this, vm => vm.InterestRate);
